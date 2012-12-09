282805efb4df3b66f8903fd21a94dded6be3560f
Endika Gutiérrez Salas <endika@Endikas-MacBook-Pro.local>
2012-12-09 16:50:41 +0100

    Basic layout created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EndikaWebsite::Application.config.secret_token = '11ed6e3a23497b57ad2347b49c8b22ae91246560c4fea58e248d50c45d50a7cb0ba9182fef5298a8d81f93de7694eff3eb290ca00d7dc47ed7fb902adade1f36'
